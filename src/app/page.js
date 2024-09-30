1ba0d4233e2a9dce7721969765b2638bb27d9993
Alexis G <116964181+Alexis-Gontier@users.noreply.github.com>
2024-09-30 12:33:16 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <Link hrf="/meteo/montrouge">Meteo: Montrouge</Link>
+      <Link href="/meteo/montrouge">Meteo: Montrouge</Link>
     </div>
   );
 }
