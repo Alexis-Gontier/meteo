d33dcfe5c33df9b60ffd90ae137ff91ffd60f4be
Alexis G <116964181+Alexis-Gontier@users.noreply.github.com>
2024-09-30 11:17:36 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,7 @@
-"use client"
-
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-
 export default function Home() {
-  const [city, setCity] = useState(''); // Pour stocker la valeur de la ville
-  const router = useRouter(); // Pour la navigation dynamique
-
-  const handleSearch = (e) => {
-    e.preventDefault(); // Empêcher le rechargement de la page
-    if (city) {
-      router.push(`/meteo/${city.toLowerCase()}`); // Redirection dynamique vers /meteo/[ville]
-    }
-  };
-
   return (
     <div>
-      <h1>Rechercher la météo</h1>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          placeholder="Entrez le nom de la ville"
-          value={city}
-          onChange={(e) => setCity(e.target.value)} // Mettre à jour la ville
-        />
-        <button type="submit">Rechercher</button>
-      </form>
+      Home
     </div>
   );
 }
